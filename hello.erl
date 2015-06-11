649add1af47b483789b25c3afb6a50758874a41c
papachan <danloaiza@yahoo.com>
2015-06-11 11:46:44 -0500

    update getpath method

--- hello.erl
+++ hello.erl
@@ -1,8 +1,13 @@
 -module(hello).
--export([hello/1]).
+-export([
+    hello/1,
+    print_path/0
+    ]).
 
 hello(0) -> [];
 hello(N) ->
 	io:fwrite("hello, world\n"),
     hello(N-1).
 
+print_path() ->
+	io:format("Current path:~p~n",[c:pwd()]).
