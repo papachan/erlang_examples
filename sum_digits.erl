2a015080d542ff982b45d6bcb9be694d5ba192cb
papachan <danloaiza@yahoo.com>
2015-11-11 22:40:00 -0500

    adding new exercise

--- sum_digits.erl
+++ sum_digits.erl
@@ -1,5 +1,10 @@
 -module(sum_digits).
+
 -compile(export_all).
+
+%% Exercise: Calculate the sum of digits of a result of a power rule as 2**n 
+%% For example,
+%% 2**15 = 32768 and the sum of its digits 3 + 2 + 7 +6 +8 is 26
  
 sum_digits(N) ->
     sum_digits(N,10).
