2d8c87a38c0a48922c47496887ed9c260b665aa2
Dan Loaiza <dan.loaiza@easytaxi.com>
2017-11-07 18:17:04 -0500

    adding another example of a rest application

---
+++ rest_cowboy_example/README.md
@@ -0,0 +1,9 @@
+new_project
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ rest_cowboy_example/apps/new_project/src/new_project_app.erl
@@ -0,0 +1,41 @@
+%%%-------------------------------------------------------------------
+%% @doc new_project public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(new_project_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%%====================================================================
+%% API
+%%====================================================================
+
+start(_StartType, _StartArgs) ->
+    {ok, Pid} = new_project_sup:start_link(),
+    Routes = [ {
+        '_',
+        [
+            {"/", new_project_root, []}
+        ]
+    } ],
+    Dispatch = cowboy_router:compile(Routes),
+
+    TransOpts = [ {ip, {0,0,0,0}}, {port, 8081} ],
+    ProtoOpts = #{env => #{dispatch => Dispatch}},
+
+    {ok, _} = cowboy:start_clear(oh_my_cowboy, TransOpts, ProtoOpts),
+
+    {ok, Pid}.
+
+
+%%--------------------------------------------------------------------
+stop(_State) ->
+    ok.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
